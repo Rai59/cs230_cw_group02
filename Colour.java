01cc254699d65bf2a729bd09a0c715d6aa58e61d
arrai59 <raiamir077@gmail.com>
2025-11-16 21:41:37 +0000

    Add actual Colour enum content

---
+++ Colour.java
@@ -0,0 +1,14 @@
+/**
+ * Represents the six colors used in Jewel Chase game tiles.
+ * Each tile has four corners, each colored with one of these colors.
+ * 
+ * @author Amir Rai
+ */
+public enum Colour {
+    RED,
+    GREEN,
+    BLUE,
+    YELLOW,
+    CYAN,
+    MAGENTA
+}
